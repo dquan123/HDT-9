2c9b456fe2acbacd4bdecc08c10439d3e473a4d7
Diego Quan <dquan@delicatessen502.com>
2025-04-29 20:09:16 -0600

    Clase del nodo de huffman

---
+++ HuffmanNode.java
@@ -0,0 +1,30 @@
+package huffman.model;
+
+public class HuffmanNode implements Comparable<HuffmanNode> {
+    public final char character;      // válido si isLeaf
+    public final int frequency;
+    public final HuffmanNode left, right;
+    public final boolean isLeaf;
+
+    // Constructor para hoja
+    public HuffmanNode(char character, int frequency) {
+        this.character = character;
+        this.frequency = frequency;
+        this.left = this.right = null;
+        this.isLeaf = true;
+    }
+
+    // Constructor para nodo interno
+    public HuffmanNode(HuffmanNode left, HuffmanNode right) {
+        this.character = '\0';
+        this.frequency = left.frequency + right.frequency;
+        this.left = left;
+        this.right = right;
+        this.isLeaf = false;
+    }
+
+    @Override
+    public int compareTo(HuffmanNode other) {
+        return Integer.compare(this.frequency, other.frequency);
+    }
+}
